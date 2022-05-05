81a6f050df7a46575883c5bb878986470b67c2c3
johntarus <tarusjohn96@gmail.com>
2022-05-05 19:43:06 +0300

    filtering

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let lastId = 0;
 
-function reducer(state = { initialState }, action) {
+export default function reducer(state = { initialState }, action) {
   switch (action.type) {
     case "bugAdded":
       return [
