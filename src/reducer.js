e0a06a928215ad7f0bda1d898036859c2b7dbede
johntarus <tarusjohn96@gmail.com>
2022-05-05 19:39:18 +0300

    filtering

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,18 @@
 let lastId = 0;
 
 function reducer(state = { initialState }, action) {
-  if (action.type === "bugAdded") {
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-    elseif(action.type === "bugResolved");
-    return state.filter((bug) => bug.id !== action.payload.id);
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
   }
   return state;
 }
